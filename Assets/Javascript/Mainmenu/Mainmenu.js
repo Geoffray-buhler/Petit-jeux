23699db0375e93437e54d4560239bd9fb81d2de3
Geoffray-buhler <geoffray.buhler@gmail.com>
2022-02-27 00:27:14 +0100

    Modification du mainmenu pour les differante scene du jeux

--- Assets/Javascript/Mainmenu/Mainmenu.js
+++ Assets/Javascript/Mainmenu/Mainmenu.js
@@ -1,5 +1,47 @@
 import SceneDeJeux from "../Inventaires/Inventaire.js";
 
+class Mainmenu extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('Mainmenu');
+    }
+
+    preload ()
+    {
+        this.load.image('sky', 'Assets/Images/Mainmenu/805177.png');
+        this.load.image('logo', 'Assets/Images/Mainmenu/Satellith.png');
+        this.load.image('btn', 'Assets/Images/Mainmenu/Jouer.png');
+        this.load.audio('musique','Assets/Sounds/Plongee-Nocturne.mp3');
+    }
+
+    create ()
+    {
+        this.add.image(500, 300, 'sky').setScale(0.3);
+        this.add.image(500, 200, 'logo').setScale(0.1);
+        var button = this.add.image(750, 500, 'btn').setScale(0.3).setInteractive();
+        var music = this.sound.add('musique').setVolume(0.2);
+        music.play();
+
+        
+        button.on('pointerover', function (event) {
+            this.setTint(0xff0000);
+        });
+
+        button.on('pointerout', function (event) {
+            this.clearTint();
+        });
+
+        button.on('pointerdown', function (pointer) {
+            this.scene.start('SceneDeJeux');
+        },this);
+
+        button.on('pointerup', function (event) {
+            this.clearTint();
+        });
+    }
+}
+
 var config = {
     type: Phaser.AUTO,
     width: 1000,
@@ -10,45 +52,11 @@ var config = {
             gravity: { y: 200 }
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        SceneDeJeux:SceneDeJeux
-    }
+    scene: [
+        Mainmenu,
+        SceneDeJeux
+    ]
+
 };
 
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('sky', 'Assets/Images/Mainmenu/805177.png');
-    this.load.image('logo', 'Assets/Images/Mainmenu/Satellith.png');
-    this.load.image('btn', 'Assets/Images/Mainmenu/Jouer.png');
-    this.load.audio('musique','Assets/Sounds/Plongee-Nocturne.mp3');
-}
-
-function create ()
-{
-    this.add.image(500, 300, 'sky').setScale(0.3);
-    this.add.image(500, 200, 'logo').setScale(0.1);
-    var button = this.add.image(750, 500, 'btn').setScale(0.3).setInteractive();
-    var music = this.sound.add('musique').setVolume(0.2);
-    music.play();
-
-    
-    button.on('pointerover', function (event) {
-        this.setTint(0xff0000);
-    });
-
-    button.on('pointerout', function (event) {
-        this.clearTint();
-    });
-
-    button.on('pointerdown', function (pointer) {
-        this.scene.start('SceneDeJeux');
-    },this);
-
-    button.on('pointerup', function (event) {
-        this.clearTint();
-    });
-}
